a5b008c479761eaf6ac7cae315f470bcf6a183c5
okcancelmaybe <okcancelmaybe@gmail.com>
2024-03-18 23:27:17 +0500

    Update portfile.cmake for my-vcpkg-test

--- ports/my-vcpkg-test/portfile.cmake
+++ ports/my-vcpkg-test/portfile.cmake
@@ -1,14 +1,16 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO "okcancelmaybe/my-vcpkg-registry"  # Adjust with the actual GitHub path
-    REF "24.3.0"
+# Define the download location and file name
+set(DOWNLOAD_URL "https://github.com/okcancelmaybe/my-vcpkg-registry/releases/download/24.3.0/my-vcpkg-test-24.3.0.zip")
+set(FILENAME "my-vcpkg-test-24.3.0.zip")
+
+# Download the .zip file
+vcpkg_download_distfile(ARCHIVE
+    URLS "${DOWNLOAD_URL}"
+    FILENAME "${FILENAME}"
     SHA512 FA416DF0760963E101B24C3D6729FE11CA99EB5DF8BB25D912B800ED13A0C264089ABECD610FCFF627C70FC226C80EBA22C954A2F3E1BBB4869B0F803C65DB07
 )
 
+# Extract the downloaded .zip file
 vcpkg_extract_source_archive_ex(
     OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE ${SOURCE_PATH}/my-vcpkg-test-24.3.0.zip
+    ARCHIVE "${ARCHIVE}"
 )
-
-file(INSTALL ${SOURCE_PATH}/include/ DESTINATION ${CURRENT_PACKAGES_DIR}/include)
-file(INSTALL ${SOURCE_PATH}/lib/ DESTINATION ${CURRENT_PACKAGES_DIR}/lib)
