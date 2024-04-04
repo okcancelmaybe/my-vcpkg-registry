0c9fb1d924d415da933c9196e7ca62d46c8f0622
okcancelmaybe <okcancelmaybe@gmail.com>
2024-04-05 00:46:13 +0500

    Update portfile.cmake

--- ports/my-vcpkg-test/portfile.cmake
+++ ports/my-vcpkg-test/portfile.cmake
@@ -15,25 +15,6 @@ vcpkg_extract_source_archive_ex(
     ARCHIVE "${ARCHIVE}"
 )
 
-
-
-# Define the download location and file name
-set(DOWNLOAD_URL "https://github.com/okcancelmaybe/my-vcpkg-registry/releases/download/24.3.0/my-vcpkg-test-24.3.0.zip")
-set(FILENAME "my-vcpkg-test-24.3.0.zip")
-
-# Download the .zip file
-vcpkg_download_distfile(ARCHIVE
-    URLS "${DOWNLOAD_URL}"
-    FILENAME "${FILENAME}"
-    SHA512 e6eb7ba2e31d91f1b5c8f258579872e764d08e4b873c43d7a4f92bbe085ddb0cf71d906c13eaf96997c70b133f06e0b3adebad6ecb68b30babe8ea5db2bb5196
-)
-
-# Extract the downloaded .zip file
-vcpkg_extract_source_archive_ex(
-    OUT_SOURCE_PATH SOURCE_PATH
-    ARCHIVE "${ARCHIVE}"
-)
-
 # Place the header file within the /include folder
 file(INSTALL
     ${SOURCE_PATH}/my-vcpkg-test.h
